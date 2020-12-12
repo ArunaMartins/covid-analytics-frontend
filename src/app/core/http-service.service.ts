47f7bc1ab20c948037206cf5ef0bd0d95b67467a
Willian Yoshiaki Kazahaya <williankaza@hotmail.com>
2020-12-12 14:39:24 -0300

    Criando serviço do estado

--- src/app/core/http-service.service.ts
+++ src/app/core/http-service.service.ts
@@ -9,7 +9,7 @@ export class HttpServiceService {
 	private 
 	constructor(private http: HttpClient) { }
 
-	setQueryParams(periodo: string, paises: string): { httpParams: HttpParams, quantidadeDias: number }{
+	private setQueryParamsPaises(periodo: string, paises: string): { httpParams: HttpParams, quantidadeDias: number }{
 		let params= new HttpParams();
 		let arrPeriodo = Generics.calcPeriodo(periodo);
 		params = params.append("paises", paises)
@@ -20,8 +20,23 @@ export class HttpServiceService {
 	}
 
 	getPaises(periodo: string, paises: Array<string> ){
-		let arrParams = this.setQueryParams(periodo, paises.join(',') )
+		let arrParams = this.setQueryParamsPaises(periodo, paises.join(',') )
 		return { observer: this.http.get('/cov/pais', { params: arrParams.httpParams }), quantDias: arrParams.quantidadeDias }
+	}
+
+	private setQueryParamsEstados(periodo: string, estados: string): { httpParams: HttpParams, quantidadeDias: number }{
+		let params= new HttpParams();
+		let arrPeriodo = Generics.calcPeriodo(periodo);
+		params = params.append("estados", estados)
+		params = params.append("periodoDe", arrPeriodo.dataInicial.toISOString().substring(0,10))
+		params = params.append("periodoAte", arrPeriodo.dataFinal.toISOString().substring(0,10))
+
+		return { httpParams: params, quantidadeDias: arrPeriodo.quant }
+	}
+
+	getEstados(periodo: string, estados: Array<string> ){
+		let arrParams = this.setQueryParamsEstados(periodo, estados.join(',') )
+		return { observer: this.http.get('/cov/estado', { params: arrParams.httpParams }), quantDias: arrParams.quantidadeDias }
 	}
 
 }

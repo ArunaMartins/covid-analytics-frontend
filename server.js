0d93f0ce3407d15514f7e0c7c67f367f24b46670
Willian Yoshiaki Kazahaya <williankaza@hotmail.com>
2020-12-14 16:48:06 -0300

    Ajuste server.js

--- server.js
+++ server.js
@@ -11,4 +11,7 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
-app.listen(8080);
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
